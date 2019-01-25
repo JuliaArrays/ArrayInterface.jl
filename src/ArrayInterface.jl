b4ef240275124a6085b483817442df3b1cc0c8f6
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2019-01-25 04:57:21 -0500

    More complex ismutable

--- src/ArrayInterface.jl
+++ src/ArrayInterface.jl
@@ -3,6 +3,15 @@ module ArrayInterface
 using Requires
 
 function ismutable end
+
+"""
+    ismutable(x::DataType)
+
+Query whether a type is mutable or not, see
+https://github.com/JuliaDiffEq/RecursiveArrayTools.jl/issues/19.
+"""
+Base.@pure ismutable(x::DataType) = x.mutable
+
 ismutable(x::Array) = true
 
 
@@ -15,6 +24,10 @@ function __init__()
 
   @require LabelledArrays="2ee39098-c373-598a-b85f-a56591580800" begin
     ismutable(x::LVector) = ismutable(x.__x)
+  end
+  
+  @require Flux="587475ba-b771-5e3f-ad9e-33799f191a9c" begin
+    ismutable(x::Flux.Tracker.TrackedArray) = false
   end
 end
 
