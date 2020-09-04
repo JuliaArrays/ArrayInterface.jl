6e9207b5f1bd2e4b1d71d829b414ecd38787b61c
Chris Elrod <elrodc@gmail.com>
2020-09-04 13:52:19 -0400

    Better sdoffsets.

--- src/stridelayout.jl
+++ src/stridelayout.jl
@@ -217,11 +217,12 @@ sdoffsets(::Any) = (Static{1}(),) # Assume arbitrary Julia data structures use 1
 @inline sdstrides(A::Array{<:Any,N}) where {N} = (Static(1), Base.tail(strides(A))...)
 @inline sdstrides(A::AbstractArray{<:Any,N}) where {N} = strides(A)
 
-@generated function maybe_static_first(x)
-    L = known_first(x)
-    isnothing(L) ? :(first(x)) : :(Static{$L}())
+@inline function sdoffsets(x, i)
+    inds = indices(x, i)
+    start = known_first(x)
+    isnothing(start) ? first(inds) : Static(start)
 end
-sdoffsets(A::AbstractArray{<:Any,N}) where {N} = ntuple(n -> maybe_static_first(axes(A,n)), Val{N}())
+sdoffsets(A::AbstractArray{<:Any,N}) where {N} = ntuple(n -> sdoffsets(A, n), Val{N}())
 
 @inline sdsize(B::Union{Transpose{T,A},Adjoint{T,A}}) where {T,A<:AbstractMatrix{T}} = permute(sdsize(parent(B)), Val{(2,1)}())
 @inline sdsize(B::PermutedDimsArray{T,N,I1,I2,A}) where {T,N,I1,I2,A<:AbstractArray{T,N}} = permute(sdsize(parent(B)), Val{I1}())
