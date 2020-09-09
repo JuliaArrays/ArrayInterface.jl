1260be6751e020b7afaaf9b42b80751c5d228de1
Chris Elrod <elrodc@gmail.com>
2020-09-09 03:36:47 -0400

    Allow all bitsinteger types.

--- src/static.jl
+++ src/static.jl
@@ -4,12 +4,15 @@ A statically sized `Int`.
 Use `Static(N)` instead of `Val(N)` when you want it to behave like a number.
 """
 struct Static{N} <: Integer
-    Static{N}()  where {N} = new{N::Int}()
+    function Static{N}()  where {N}
+        @assert isa(typeof(N), Base.BitIntegerType) "$N is not a primitive integer type"
+        return new{N}()
+    end
 end
-Base.@pure Static(N::Int) = Static{N}()
-Static(N) = Static(convert(Int,N))
+Base.@pure Static(N::Union{Int8, Int16, Int32, Int64, Int128, UInt8, UInt16, UInt32, UInt64, UInt128}) = Static{N}()
+Static(N) = Static(convert(Int, N))
 Static(::Val{N}) where {N} = Static{N}()
-@inline Base.Val(::Static{N}) where {N} = Val{N}()
+Base.Val(::Static{N}) where {N} = Val{N}()
 Base.convert(::Type{T}, ::Static{N}) where {T<:Number,N} = convert(T, N)
 Base.convert(::Type{Static{N}}, ::Static{N}) where {N} = Static{N}()
 Base.promote_rule(::Type{<:Static}, ::Type{T}) where {T} = promote_rule(Int, T)
@@ -17,53 +20,62 @@ Base.promote_rule(::Type{T}, ::Type{<:Static}) where {T} = promote_rule(T, Int)
 Base.promote_rule(::Type{<:Static}, ::Type{<:Static}) where {T} = Int
 Base.:(%)(::Static{N}, ::Type{Integer}) where {N} = N
 
-@inline Base.iszero(::Static{0}) = true
-@inline Base.iszero(::Static) = false
+@inline Base.iszero(::Static{M}) where {M} = iszero(M)
+@inline Base.isone(::Static{M}) where {M} = isone(M)
 
-Base.:(+)(i::Number, ::Static{0}) = i
-Base.:(+)(::Static{0}, i::Number) = i
-Base.:(+)(::Static{0}, i::Integer) = i
-Base.:(+)(::Static{0}, ::Static{0}) = Static{0}()
-Base.:(+)(::Static{N}, ::Static{0}) where {N} = Static{N}()
-Base.:(+)(::Static{0}, ::Static{N}) where {N} = Static{N}()
-Base.:(+)(::Static{M}, ::Static{N}) where {M,N} = Static{M + N}()
-
-Base.:(-)(::Static{0}, i::Number) = -i
-Base.:(-)(i::Number, ::Static{0}) = i
-Base.:(-)(::Static{0}, ::Static{0}) = Static{0}()
-Base.:(-)(::Static{0}, ::Static{N}) where {N} = Static{-N}()
-Base.:(-)(::Static{N}, ::Static{0}) where {N} = Static{N}()
-Base.:(-)(::Static{M}, ::Static{N}) where {M,N} = Static{M - N}()
-
-Base.:(*)(::Static{0}, i::Number) = Static{0}()
-Base.:(*)(i::Number, ::Static{0}) = Static{0}()
-Base.:(*)(::Static{0}, ::Static{M}) where {M} = Static{0}()
-Base.:(*)(::Static{M}, ::Static{0}) where {M} = Static{0}()
-Base.:(*)(::Static{0}, ::Static{0}) = Static{0}()
-Base.:(*)(::Static{1}, i::Number) = i
-Base.:(*)(i::Number, ::Static{1}) = i
-Base.:(*)(::Static{0}, ::Static{1}) where {M} = Static{0}()
-Base.:(*)(::Static{1}, ::Static{0}) where {M} = Static{0}()
-Base.:(*)(::Static{M}, ::Static{1}) where {M} = Static{M}()
-Base.:(*)(::Static{1}, ::Static{M}) where {M} = Static{M}()
-Base.:(*)(::Static{1}, ::Static{1}) = Static{1}()
-Base.:(*)(::Static{M}, ::Static{N}) where {M,N} = Static{M * N}()
-
-Base.:(÷)(::Static{M}, ::Static{N}) where {M,N} = Static{M ÷ N}()
-Base.:(%)(::Static{M}, ::Static{N}) where {M,N} = Static{M % N}()
-Base.:(<<)(::Static{M}, ::Static{N}) where {M,N} = Static{M << N}()
-Base.:(>>)(::Static{M}, ::Static{N}) where {M,N} = Static{M >> N}()
-Base.:(>>>)(::Static{M}, ::Static{N}) where {M,N} = Static{M >>> N}()
-Base.:(&)(::Static{M}, ::Static{N}) where {M,N} = Static{M & N}()
-Base.:(|)(::Static{M}, ::Static{N}) where {M,N} = Static{M | N}()
-Base.:(⊻)(::Static{M}, ::Static{N}) where {M,N} = Static{M ⊻ N}()
-
-Base.:(==)(::Static{M}, ::Static{N}) where {M,N} = false
-Base.:(==)(::Static{M}, ::Static{M}) where {M} = true
-Base.:(≤)(::Static{M}, N::Int) where {M} = M ≤ N
-Base.:(≤)(N::Int, ::Static{M}) where {M} = N ≤ M
-Base.:(≥)(::Static{M}, N::Int) where {M} = M ≤ N
-Base.:(≥)(N::Int, ::Static{M}) where {M} = N ≥ M
+for T ∈ [:Any, :Number]
+    @eval begin
+        @inline function Base.:(+)(i::$T, ::Static{M}) where {M}
+            if iszero(M)
+                i
+            else
+                i + M
+            end
+        end
+        @inline function Base.:(+)(::Static{M}, i::$T) where {M}
+            if iszero(M)
+                i
+            else
+                M + i
+            end
+        end
+        @inline function Base.:(-)(i::$T, ::Static{M}) where {M}
+            if iszero(M)
+                i
+            else
+                i - M
+            end
+        end
+        @inline function Base.:(*)(i::$T, j::Static{M}) where {M}
+            if iszero(M)
+                j
+            elseif isone(M)
+                i
+            else
+                i * M
+            end
+        end
+        @inline function Base.:(*)(j::Static{M}, i::$T) where {M}
+            if iszero(M)
+                j
+            elseif isone(M)
+                i
+            else
+                M * i
+            end
+        end
+    end
+end
+for f ∈ [:(+), :(-), :(*), :(/), :(÷), :(%), :(<<), :(>>), :(>>>), :(&), :(|), :(⊻)]
+    @eval @generated Base.$f(::Static{M}, ::Static{N}) where {M,N} = Expr(:call, Expr(:curly, :Static, $f(M, N)))
+end
+for f ∈ [:(==), :(!=), :(<), :(≤), :(>), :(≥)]
+    @eval begin
+        @inline Base.$f(::Static{M}, ::Static{N}) where {M,N} = $f(M, N)
+        @inline Base.$f(::Static{M}, x::Integer) where {M} = $f(M, x)
+        @inline Base.$f(x::Integer, ::Static{M}) where {M} = $f(x, M)
+    end
+end
 
 @inline function maybe_static(f::F, g::G, x) where {F, G}
     L = f(x)
