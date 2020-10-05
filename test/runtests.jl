8100a848560424684122bc815e61cb94a2efc6df
Chris Elrod <elrodc@gmail.com>
2020-10-05 15:14:50 -0400

    Fix insert and deleteat boundscheck for tuples. Improve unsafe_deleteat performance for single- and two-element tuples.

--- test/runtests.jl
+++ test/runtests.jl
@@ -463,13 +463,13 @@ end
     @test @inferred(ArrayInterface.deleteat([1, 2, 3], [2, 3])) == [1]
 
 
-    @test @inferred(ArrayInterface.insert((1,2,3), 1, -2)) == (-2, 1, 2, 3)
-    @test @inferred(ArrayInterface.insert((1,2,3), 2, -2)) == (1, -2, 2, 3)
-    @test @inferred(ArrayInterface.insert((1,2,3), 3, -2)) == (1, 2, -2, 3)
+    @test @inferred(ArrayInterface.insert((2,3,4), 1, -2)) == (-2, 2, 3, 4)
+    @test @inferred(ArrayInterface.insert((2,3,4), 2, -2)) == (2, -2, 3, 4)
+    @test @inferred(ArrayInterface.insert((2,3,4), 3, -2)) == (2, 3, -2, 4)
 
-    @test @inferred(ArrayInterface.deleteat((1, 2, 3), 1)) == (2, 3)
-    @test @inferred(ArrayInterface.deleteat((1, 2, 3), 2)) == (1, 3)
-    @test @inferred(ArrayInterface.deleteat((1, 2, 3), 3)) == (1, 2)
+    @test @inferred(ArrayInterface.deleteat((2, 3, 4), 1)) == (3, 4)
+    @test @inferred(ArrayInterface.deleteat((2, 3, 4), 2)) == (2, 4)
+    @test @inferred(ArrayInterface.deleteat((2, 3, 4), 3)) == (2, 3)
     @test ArrayInterface.deleteat((1, 2, 3), [1, 2]) == (3,)
 end
 
