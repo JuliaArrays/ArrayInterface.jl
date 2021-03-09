62b6f7a97ed660ecb2f784ce3fdec99048210121
Zachary P. Christensen <zchristensen7@gmail.com>
2021-03-08 19:52:27 -0500

    docstrings + try to preserve static lengths

--- src/broadcast.jl
+++ src/broadcast.jl
@@ -1,5 +1,9 @@
 
-""" BroadcastAxis """
+"""
+    BroadcastAxis
+
+An abstract trait that is used to determine how axes are combined when calling `broadcast_axis`.
+"""
 abstract type BroadcastAxis end
 
 struct BroadcastAxisDefault <: BroadcastAxis end
@@ -7,7 +11,21 @@ struct BroadcastAxisDefault <: BroadcastAxis end
 BroadcastAxis(x) = BroadcastAxis(typeof(x))
 BroadcastAxis(::Type{T}) where {T} = BroadcastAxisDefault()
 
-""" broadcast_axis(x, y) """
+"""
+    broadcast_axis(x, y)
+
+Broadcast axis `x` and `y` into a common space. The resulting axis should be equal in length
+to both `x` and `y` unless one has a length of `1`, in which case the longest axis will be
+equal to the output.
+
+```julia
+julia> ArrayInterface.broadcast_axis(1:10, 1:10)
+
+julia> ArrayInterface.broadcast_axis(1:10, 1)
+1:10
+
+```
+"""
 broadcast_axis(x, y) = broadcast_axis(BroadcastAxis(x), x, y)
 # stagger default broadcasting in case y has something other than default
 broadcast_axis(::BroadcastAxisDefault, x, y) = _broadcast_axis(BroadcastAxis(y), x, y)
@@ -15,9 +33,16 @@ function _broadcast_axis(::BroadcastAxisDefault, x, y)
     return One():_combine_length(static_length(x), static_length(y))
 end
 _broadcast_axis(s::BroadcastAxis, x, y) = broadcasted_axis(s, x, y)
-_combine_length(::StaticInt{X}, ::StaticInt{Y}) where {X,Y} = static(_combine_length(X, Y))
-_combine_length(::StaticInt{X}, y::Int) where {X} = _combine_length(X, y)
-_combine_length(x::Int, ::StaticInt{Y}) where {Y} = _combine_length(x, Y)
+
+# we can use a similar trick as we do with `indices` where unequal sizes error and we just
+# keep the static value. However, axes can be unequal if one of them is `1` so we have to
+# fall back to dynamic values in those cases
+_combine_length(x::StaticInt{X}, y::StaticInt{Y}) where {X,Y} = static(_combine_length(X, Y))
+_combine_length(x::StaticInt{X}, ::Int) where {X} = x
+_combine_length(x::StaticInt{1}, y::Int) = y
+_combine_length(x::StaticInt{1}, y::StaticInt{1}) = y
+_combine_length(x::Int, y::StaticInt{Y}) where {Y} = y
+_combine_length(x::Int, y::StaticInt{1}) = x
 @inline function _combine_length(x::Int, y::Int)
     if x === y
         return x
