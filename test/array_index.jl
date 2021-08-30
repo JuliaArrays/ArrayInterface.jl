afa63d4a9f32856e3085e32fb36091594302ba3c
Zachary P. Christensen <zchristensen7@gmail.com>
2021-08-30 07:00:00 -0400

    Moved ArrayIndex tests to "test/array_index.jl"

---
+++ test/array_index.jl
@@ -0,0 +1,17 @@
+
+A = zeros(3, 4, 5);
+A[:] = 1:60
+Ap = @view(PermutedDimsArray(A,(3,1,2))[:,1:2,1])';
+
+ap_index = ArrayInterface.StrideIndex(Ap)
+for x_i in axes(Ap, 1)
+    for y_i in axes(Ap, 2)
+        @test ap_index[x_i, y_i] == ap_index[x_i, y_i]
+    end
+end
+@test @inferred(ArrayInterface.known_offsets(ap_index)) === ArrayInterface.known_offsets(Ap)
+@test @inferred(ArrayInterface.known_offset1(ap_index)) === ArrayInterface.known_offset1(Ap)
+@test @inferred(ArrayInterface.known_strides(ap_index)) === ArrayInterface.known_strides(Ap)
+@test @inferred(ArrayInterface.contiguous_axis(ap_index)) == 1
+@test @inferred(ArrayInterface.stride_rank(ap_index)) == (1, 3)
+
