7a580186f8974d31925a433b4bd993d9881838c5
Chris Elrod <elrodc@gmail.com>
2021-10-25 01:00:19 -0400

    add eachindex(::OptionallyStaticUnitRange) tests

--- test/ranges.jl
+++ test/ranges.jl
@@ -106,5 +106,8 @@
     @test ArrayInterface.static_step(Base.OneTo(one(UInt))) === static(1)
     
     @test Base.setindex(1:5, [6,2], 1:2) == [6,2,3,4,5]
+
+    @test @inferred(eachindex(static(-7):static(-7))) === static(1):static(15)
+    @test @inferred((static(-7):static(7))[first(eachindex(static(-7):static(7)))]) == -7
 end
 
