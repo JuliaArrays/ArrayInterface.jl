df4b029fda9d2434742508bf1c222c8c82725030
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-05-16 07:09:20 -0400

    Fix test runners and see if tests pass

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,24 @@
-using ArrayInterface
-using Test
+using Test, Pkg
 
+const GROUP = get(ENV, "GROUP", "All")
+
+function dev_subpkg(subpkg)
+    subpkg_path = joinpath(dirname(@__DIR__), "lib", subpkg)
+    Pkg.develop(PackageSpec(path=subpkg_path))
+end
+
+function activate_subpkg_env(subpkg)
+    subpkg_path = joinpath(dirname(@__DIR__), "lib", subpkg)
+    Pkg.activate(subpkg_path)
+    Pkg.develop(PackageSpec(path=subpkg_path))
+    Pkg.instantiate()
+end
+
+@time begin
+if GROUP == "HighLevel"
+    # Any tests for the level combined package? None right now.
+else
+    dev_subpkg(GROUP)
+    subpkg_path = joinpath(dirname(@__DIR__), "lib", GROUP)
+    Pkg.test(PackageSpec(name=GROUP, path=subpkg_path))
+end
