3dc43086bdecff90ef43f41a0a4cf66cb464430e
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-05-21 11:00:23 -0400

    fix namespace

--- lib/ArrayInterfaceGPUArrays/src/ArrayInterfaceGPUArrays.jl
+++ lib/ArrayInterfaceGPUArrays/src/ArrayInterfaceGPUArrays.jl
@@ -1,24 +1,24 @@
 module ArrayInterfaceGPUArrays
 
 using Adapt
-using ArrayInterface
+using ArrayInterfaceCore
 using GPUArrays
 
-ArrayInterface.fast_scalar_indexing(::Type{<:GPUArrays.AbstractGPUArray}) = false
-@inline ArrayInterface.allowed_getindex(x::GPUArrays.AbstractGPUArray, i...) = CUDA.@allowscalar(x[i...])
-@inline ArrayInterface.allowed_setindex!(x::GPUArrays.AbstractGPUArray, v, i...) = (CUDA.@allowscalar(x[i...] = v))
+ArrayInterfaceCore.fast_scalar_indexing(::Type{<:GPUArrays.AbstractGPUArray}) = false
+@inline ArrayInterfaceCore.allowed_getindex(x::GPUArrays.AbstractGPUArray, i...) = GPUArrays.@allowscalar(x[i...])
+@inline ArrayInterfaceCore.allowed_setindex!(x::GPUArrays.AbstractGPUArray, v, i...) = (GPUArrays.@allowscalar(x[i...] = v))
 
 function Base.setindex(x::GPUArrays.AbstractGPUArray, v, i::Int)
     _x = copy(x)
-    ArrayInterface.allowed_setindex!(_x, v, i)
+    ArrayInterfaceCore.allowed_setindex!(_x, v, i)
     return _x
 end
 
-function ArrayInterface.restructure(x::GPUArrays.AbstractGPUArray, y)
-    reshape(Adapt.adapt(ArrayInterface.parameterless_type(x), y), Base.size(x)...)
+function ArrayInterfaceCore.restructure(x::GPUArrays.AbstractGPUArray, y)
+    reshape(Adapt.adapt(ArrayInterfaceCore.parameterless_type(x), y), Base.size(x)...)
 end
 
-function ArrayInterface.lu_instance(A::GPUArrays.AbstractGPUMatrix{T}) where {T}
+function ArrayInterfaceCore.lu_instance(A::GPUArrays.AbstractGPUMatrix{T}) where {T}
     qr(similar(A, 1, 1))
 end
 
