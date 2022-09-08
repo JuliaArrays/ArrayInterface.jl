1dabc4ab249b9e635b012df0f53b2478425a221b
TheZombie1999 <ninja110tim@hotmail.de>
2022-09-08 16:40:17 +0200

    Add a test for the lu_instance function

--- lib/ArrayInterfaceCUDA/test/runtests.jl
+++ lib/ArrayInterfaceCUDA/test/runtests.jl
@@ -1,1 +1,10 @@
+
+using CUDA
+using ArrayInterface
 using ArrayInterfaceCUDA
+using LinearAlgebra
+
+using Test
+
+# Test whether lu_instance throws an error when invoked with an gpu array
+@test !isa(try ArrayInterface.lu_instance(CUDA.CuArray([1.f0 1.f0; 1.f0 1.f0])) catch ex ex end, Exception) 
