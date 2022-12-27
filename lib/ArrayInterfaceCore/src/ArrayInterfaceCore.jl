f3ce09324b219ae9b4538c8efc82aeca9795bd10
Chris Elrod <elrodc@gmail.com>
2022-12-27 16:42:01 -0500

    `indices_do_not_alias` was ambiguous (#370)
    * `indices_do_not_alias` was ambiguous
    
    * add .JuliaFormatter.toml files

--- lib/ArrayInterfaceCore/src/ArrayInterfaceCore.jl
+++ lib/ArrayInterfaceCore/src/ArrayInterfaceCore.jl
@@ -18,14 +18,14 @@ else
         end
     end
 end
-@assume_effects :total __parameterless_type(T) = Base.typename(T).wrapper
+@assume_effects :total __parameterless_type(T)=Base.typename(T).wrapper
 parameterless_type(x) = parameterless_type(typeof(x))
 parameterless_type(x::Type) = __parameterless_type(x)
 
-const VecAdjTrans{T,V<:AbstractVector{T}} = Union{Transpose{<:Any,V},Adjoint{<:Any,V}}
-const MatAdjTrans{T,M<:AbstractMatrix{T}} = Union{Transpose{<:Any,M},Adjoint{<:Any,M}}
-const UpTri{T,M} = Union{UpperTriangular{T,M},UnitUpperTriangular{T,M}}
-const LoTri{T,M} = Union{LowerTriangular{T,M},UnitLowerTriangular{T,M}}
+const VecAdjTrans{T, V <: AbstractVector{T}} = Union{Transpose{<:Any, V}, Adjoint{<:Any, V}}
+const MatAdjTrans{T, M <: AbstractMatrix{T}} = Union{Transpose{<:Any, M}, Adjoint{<:Any, M}}
+const UpTri{T, M} = Union{UpperTriangular{T, M}, UnitUpperTriangular{T, M}}
+const LoTri{T, M} = Union{LowerTriangular{T, M}, UnitLowerTriangular{T, M}}
 
 """
     ArrayInterfaceCore.map_tuple_type(f, T::Type{<:Tuple})
@@ -46,7 +46,7 @@ if VERSION >= v"1.8"
         ntuple(i -> f(fieldtype(T, i)), Val{fieldcount(T)}())
     end
 else
-    function map_tuple_type(f::F, ::Type{T}) where {F,T<:Tuple}
+    function map_tuple_type(f::F, ::Type{T}) where {F, T <: Tuple}
         if @generated
             t = Expr(:tuple)
             for i in 1:fieldcount(T)
@@ -90,7 +90,9 @@ if VERSION >= v"1.8"
     end
     _new_field_positions(::Tuple{}) = ()
     @nospecialize
-    _new_field_positions(x::Tuple) = (_fl1(x, x[1])..., _new_field_positions(Base.tail(x))...)
+    function _new_field_positions(x::Tuple)
+        (_fl1(x, x[1])..., _new_field_positions(Base.tail(x))...)
+    end
     _fl1(x::Tuple, x1::Tuple) = ntuple(Base.Fix1(tuple, length(x) - 1), Val(length(x1)))
     _fl1(x::Tuple, x1) = ((length(x) - 1, 0),)
     @specialize
@@ -114,8 +116,12 @@ else
         end
     end
     _flatten(::Tuple{}) = ()
-    @inline _flatten(t::Tuple{Any,Vararg{Any}}) = (getfield(t, 1), _flatten(Base.tail(t))...)
-    @inline _flatten(t::Tuple{Tuple,Vararg{Any}}) = (getfield(t, 1)..., _flatten(Base.tail(t))...)
+    @inline function _flatten(t::Tuple{Any, Vararg{Any}})
+        (getfield(t, 1), _flatten(Base.tail(t))...)
+    end
+    @inline function _flatten(t::Tuple{Tuple, Vararg{Any}})
+        (getfield(t, 1)..., _flatten(Base.tail(t))...)
+    end
 end
 
 """
@@ -124,15 +130,17 @@ end
 Returns the parent array that type `T` wraps.
 """
 parent_type(x) = parent_type(typeof(x))
-parent_type(@nospecialize T::Type{<:Union{Symmetric,Hermitian}}) = fieldtype(T, :data)
-parent_type(@nospecialize T::Type{<:Union{UpTri,LoTri}}) = fieldtype(T, :data)
+parent_type(@nospecialize T::Type{<:Union{Symmetric, Hermitian}}) = fieldtype(T, :data)
+parent_type(@nospecialize T::Type{<:Union{UpTri, LoTri}}) = fieldtype(T, :data)
 parent_type(@nospecialize T::Type{<:PermutedDimsArray}) = fieldtype(T, :parent)
 parent_type(@nospecialize T::Type{<:Adjoint}) = fieldtype(T, :parent)
 parent_type(@nospecialize T::Type{<:Transpose}) = fieldtype(T, :parent)
 parent_type(@nospecialize T::Type{<:SubArray}) = fieldtype(T, :parent)
 parent_type(@nospecialize T::Type{<:Base.ReinterpretArray}) = fieldtype(T, :parent)
 parent_type(@nospecialize T::Type{<:Base.ReshapedArray}) = fieldtype(T, :parent)
-parent_type(@nospecialize T::Type{<:Union{Base.Slice,Base.IdentityUnitRange}}) = fieldtype(T, :indices)
+function parent_type(@nospecialize T::Type{<:Union{Base.Slice, Base.IdentityUnitRange}})
+    fieldtype(T, :indices)
+end
 parent_type(@nospecialize T::Type{<:Diagonal}) = fieldtype(T, :diag)
 parent_type(T::Type) = T
 
@@ -145,7 +153,9 @@ type changing to `promote_type(T,T2)`.
 Note that no generic fallback is given.
 """
 function promote_eltype end
-promote_eltype(::Type{Array{T,N}}, ::Type{T2}) where {T,T2,N} = Array{promote_type(T,T2),N}
+function promote_eltype(::Type{Array{T, N}}, ::Type{T2}) where {T, T2, N}
+    Array{promote_type(T, T2), N}
+end
 
 """
     buffer(x)
@@ -156,7 +166,7 @@ may not return another array type.
 buffer(x) = parent(x)
 buffer(x::SparseMatrixCSC) = getfield(x, :nzval)
 buffer(x::SparseVector) = getfield(x, :nzval)
-buffer(@nospecialize x::Union{Base.Slice,Base.IdentityUnitRange}) = getfield(x, :indices)
+buffer(@nospecialize x::Union{Base.Slice, Base.IdentityUnitRange}) = getfield(x, :indices)
 
 """
     is_forwarding_wrapper(::Type{T}) -> Bool
@@ -197,12 +207,12 @@ julia> ArrayInterfaceCore.GetIndex{false}(1:10)(11)  # shouldn't be in-bounds
 ```
 
 """
-struct GetIndex{CB,B} <: Function
+struct GetIndex{CB, B} <: Function
     buffer::B
 
-    GetIndex{true,B}(b) where {B} = new{true,B}(b)
-    GetIndex{false,B}(b) where {B} = new{false,B}(b)
-    GetIndex{check}(b::B) where {check,B} = GetIndex{check,B}(b)
+    GetIndex{true, B}(b) where {B} = new{true, B}(b)
+    GetIndex{false, B}(b) where {B} = new{false, B}(b)
+    GetIndex{check}(b::B) where {check, B} = GetIndex{check, B}(b)
     GetIndex(b) = GetIndex{true}(b)
 end
 
@@ -235,16 +245,16 @@ julia> x[2]
 
 ```
 """
-struct SetIndex!{CB,B} <: Function
+struct SetIndex!{CB, B} <: Function
     buffer::B
 
-    SetIndex!{true,B}(b) where {B} = new{true,B}(b)
-    SetIndex!{false,B}(b) where {B} = new{false,B}(b)
-    SetIndex!{check}(b::B) where {check,B} = SetIndex!{check,B}(b)
+    SetIndex!{true, B}(b) where {B} = new{true, B}(b)
+    SetIndex!{false, B}(b) where {B} = new{false, B}(b)
+    SetIndex!{check}(b::B) where {check, B} = SetIndex!{check, B}(b)
     SetIndex!(b) = SetIndex!{true}(b)
 end
 
-buffer(x::Union{SetIndex!,GetIndex}) = getfield(x, :buffer)
+buffer(x::Union{SetIndex!, GetIndex}) = getfield(x, :buffer)
 
 Base.@propagate_inbounds @inline (g::GetIndex{true})(inds...) = buffer(g)[inds...]
 @inline (g::GetIndex{false})(inds...) = @inbounds(buffer(g)[inds...])
@@ -276,7 +286,7 @@ Query whether instances of type `T` are mutable or not, see
 https://github.com/JuliaDiffEq/RecursiveArrayTools.jl/issues/19.
 """
 ismutable(x) = ismutable(typeof(x))
-function ismutable(::Type{T}) where {T<:AbstractArray}
+function ismutable(::Type{T}) where {T <: AbstractArray}
     if parent_type(T) <: T
         return true
     else
@@ -312,7 +322,7 @@ can_setindex(::Type{<:AbstractDict}) = true
 can_setindex(::Type{<:Base.ImmutableDict}) = false
 can_setindex(@nospecialize T::Type{<:Tuple}) = false
 can_setindex(@nospecialize T::Type{<:NamedTuple}) = false
-can_setindex(::Type{<:Base.Iterators.Pairs{<:Any,<:Any,P}}) where {P} = can_setindex(P)
+can_setindex(::Type{<:Base.Iterators.Pairs{<:Any, <:Any, P}}) where {P} = can_setindex(P)
 
 """
     aos_to_soa(x)
@@ -357,17 +367,17 @@ has_sparsestruct(::Type{<:SymTridiagonal}) = true
 Determine whether a given abstract matrix is singular.
 """
 issingular(A::AbstractMatrix) = issingular(Matrix(A))
-issingular(A::AbstractSparseMatrix) = !issuccess(lu(A, check=false))
-issingular(A::Matrix) = !issuccess(lu(A, check=false))
+issingular(A::AbstractSparseMatrix) = !issuccess(lu(A, check = false))
+issingular(A::Matrix) = !issuccess(lu(A, check = false))
 issingular(A::UniformScaling) = A.λ == 0
 issingular(A::Diagonal) = any(iszero, A.diag)
 issingular(A::Bidiagonal) = any(iszero, A.dv)
 issingular(A::SymTridiagonal) = diaganyzero(ldlt(A).data)
-issingular(A::Tridiagonal) = !issuccess(lu(A, check=false))
-issingular(A::Union{Hermitian,Symmetric}) = diaganyzero(bunchkaufman(A, check=false).LD)
-issingular(A::Union{LowerTriangular,UpperTriangular}) = diaganyzero(A.data)
-issingular(A::Union{UnitLowerTriangular,UnitUpperTriangular}) = false
-issingular(A::Union{Adjoint,Transpose}) = issingular(parent(A))
+issingular(A::Tridiagonal) = !issuccess(lu(A, check = false))
+issingular(A::Union{Hermitian, Symmetric}) = diaganyzero(bunchkaufman(A, check = false).LD)
+issingular(A::Union{LowerTriangular, UpperTriangular}) = diaganyzero(A.data)
+issingular(A::Union{UnitLowerTriangular, UnitUpperTriangular}) = false
+issingular(A::Union{Adjoint, Transpose}) = issingular(parent(A))
 diaganyzero(A) = any(iszero, view(A, diagind(A)))
 
 """
@@ -389,7 +399,7 @@ function findstructralnz(x::Bidiagonal)
     (rowind, colind)
 end
 
-function findstructralnz(x::Union{Tridiagonal,SymTridiagonal})
+function findstructralnz(x::Union{Tridiagonal, SymTridiagonal})
     n = Base.size(x, 1)
     rowind = TridiagonalIndex(n + n - 1 + n - 1, n, true)
     colind = TridiagonalIndex(n + n - 1 + n - 1, n, false)
@@ -411,7 +421,11 @@ colors of the matrix.
 """
 fast_matrix_colors(A) = false
 fast_matrix_colors(A::AbstractArray) = fast_matrix_colors(typeof(A))
-fast_matrix_colors(A::Type{<:Union{Diagonal,Bidiagonal,Tridiagonal,SymTridiagonal}}) = true
+function fast_matrix_colors(A::Type{
+                                    <:Union{Diagonal, Bidiagonal, Tridiagonal,
+                                            SymTridiagonal}})
+    true
+end
 
 """
     matrix_colors(A::Union{Array,UpperTriangular,LowerTriangular})
@@ -419,12 +433,12 @@ fast_matrix_colors(A::Type{<:Union{Diagonal,Bidiagonal,Tridiagonal,SymTridiagona
 The color vector for dense matrix and triangular matrix is simply
 `[1,2,3,..., Base.size(A,2)]`.
 """
-function matrix_colors(A::Union{Array,UpperTriangular,LowerTriangular})
+function matrix_colors(A::Union{Array, UpperTriangular, LowerTriangular})
     eachindex(1:Base.size(A, 2)) # Vector Base.size matches number of rows
 end
 matrix_colors(A::Diagonal) = fill(1, Base.size(A, 2))
 matrix_colors(A::Bidiagonal) = _cycle(1:2, Base.size(A, 2))
-matrix_colors(A::Union{Tridiagonal,SymTridiagonal}) = _cycle(1:3, Base.size(A, 2))
+matrix_colors(A::Union{Tridiagonal, SymTridiagonal}) = _cycle(1:3, Base.size(A, 2))
 _cycle(repetend, len) = repeat(repetend, div(len, length(repetend)) + 1)[1:len]
 
 """
@@ -442,20 +456,16 @@ function lu_instance(A::Matrix{T}) where {T}
 end
 function lu_instance(jac_prototype::SparseMatrixCSC)
     @static if VERSION < v"1.9.0-DEV.1622"
-        SuiteSparse.UMFPACK.UmfpackLU(
-            Ptr{Cvoid}(),
-            Ptr{Cvoid}(),
-            1,
-            1,
-            jac_prototype.colptr[1:1],
-            jac_prototype.rowval[1:1],
-            jac_prototype.nzval[1:1],
-            0,
-        )
+        SuiteSparse.UMFPACK.UmfpackLU(Ptr{Cvoid}(),
+                                      Ptr{Cvoid}(),
+                                      1,
+                                      1,
+                                      jac_prototype.colptr[1:1],
+                                      jac_prototype.rowval[1:1],
+                                      jac_prototype.nzval[1:1],
+                                      0)
     else
-        SuiteSparse.UMFPACK.UmfpackLU(
-            similar(jac_prototype, 1, 1)
-        )
+        SuiteSparse.UMFPACK.UmfpackLU(similar(jac_prototype, 1, 1))
     end
 end
 
@@ -471,7 +481,7 @@ lu_instance(a::Number) = a
 
 Returns the number.
 """
-lu_instance(a::Any) = lu(a, check=false)
+lu_instance(a::Any) = lu(a, check = false)
 
 """
     safevec(v)
@@ -563,9 +573,9 @@ Otherwise, returns `nothing`.
 device(A) = device(typeof(A))
 device(::Type) = nothing
 device(::Type{<:Tuple}) = CPUTuple()
-device(::Type{T}) where {T<:Array} = CPUPointer()
-device(::Type{T}) where {T<:AbstractArray} = _device(parent_type(T), T)
-function _device(::Type{P}, ::Type{T}) where {P,T}
+device(::Type{T}) where {T <: Array} = CPUPointer()
+device(::Type{T}) where {T <: AbstractArray} = _device(parent_type(T), T)
+function _device(::Type{P}, ::Type{T}) where {P, T}
     if defines_strides(T)
         return device(P)
     else
@@ -574,7 +584,7 @@ function _device(::Type{P}, ::Type{T}) where {P,T}
 end
 _not_pointer(::CPUPointer) = CPUIndex()
 _not_pointer(x) = x
-_device(::Type{T}, ::Type{T}) where {T<:DenseArray} = CPUPointer()
+_device(::Type{T}, ::Type{T}) where {T <: DenseArray} = CPUPointer()
 _device(::Type{T}, ::Type{T}) where {T} = CPUIndex()
 
 """
@@ -651,9 +661,9 @@ struct TridiagonalIndex <: MatrixIndex
     isrow::Bool
 end
 
-Base.firstindex(i::Union{BidiagonalIndex,TridiagonalIndex}) = 1
-Base.lastindex(i::Union{BidiagonalIndex,TridiagonalIndex}) = i.count
-Base.length(i::Union{BidiagonalIndex,TridiagonalIndex}) = lastindex(i)
+Base.firstindex(i::Union{BidiagonalIndex, TridiagonalIndex}) = 1
+Base.lastindex(i::Union{BidiagonalIndex, TridiagonalIndex}) = i.count
+Base.length(i::Union{BidiagonalIndex, TridiagonalIndex}) = lastindex(i)
 
 Base.@propagate_inbounds function Base.getindex(ind::BidiagonalIndex, i::Int)
     @boundscheck 1 <= i <= ind.count || throw(BoundsError(ind, i))
@@ -700,7 +710,7 @@ known_first(T::Type) = is_forwarding_wrapper(T) ? known_first(parent_type(T)) : 
 known_first(::Type{<:Base.OneTo}) = 1
 known_first(@nospecialize T::Type{<:LinearIndices}) = 1
 known_first(@nospecialize T::Type{<:Base.IdentityUnitRange}) = known_first(parent_type(T))
-function known_first(::Type{<:CartesianIndices{N,R}}) where {N,R}
+function known_first(::Type{<:CartesianIndices{N, R}}) where {N, R}
     _cartesian_index(ntuple(i -> known_first(R.parameters[i]), Val(N)))
 end
 
@@ -721,7 +731,7 @@ julia> ArrayInterfaceCore.known_first(typeof(static(1):static(4)))
 """
 known_last(x) = known_last(typeof(x))
 known_last(T::Type) = is_forwarding_wrapper(T) ? known_last(parent_type(T)) : nothing
-function known_last(::Type{<:CartesianIndices{N,R}}) where {N,R}
+function known_last(::Type{<:CartesianIndices{N, R}}) where {N, R}
     _cartesian_index(ntuple(i -> known_last(R.parameters[i]), Val(N)))
 end
 
@@ -776,7 +786,7 @@ julia> ArrayInterfaceCore.ndims_index([CartesianIndex(1, 2), CartesianIndex(1, 3
 """
 ndims_index(::Type{<:Base.AbstractCartesianIndex{N}}) where {N} = N
 # preserve CartesianIndices{0} as they consume a dimension.
-ndims_index(::Type{CartesianIndices{0,Tuple{}}}) = 1
+ndims_index(::Type{CartesianIndices{0, Tuple{}}}) = 1
 ndims_index(@nospecialize T::Type{<:AbstractArray{Bool}}) = ndims(T)
 ndims_index(@nospecialize T::Type{<:AbstractArray}) = ndims_index(eltype(T))
 ndims_index(@nospecialize T::Type{<:Base.LogicalIndex}) = ndims(fieldtype(T, :mask))
@@ -804,12 +814,12 @@ julia> ndims(CartesianIndices((2,2))[[CartesianIndex(1, 1), CartesianIndex(1, 2)
 ndims_shape(T::DataType) = ndims_index(T)
 ndims_shape(::Type{Colon}) = 1
 ndims_shape(@nospecialize T::Type{<:CartesianIndices}) = ndims(T)
-ndims_shape(@nospecialize T::Type{<:Union{Number,Base.AbstractCartesianIndex}}) = 0
+ndims_shape(@nospecialize T::Type{<:Union{Number, Base.AbstractCartesianIndex}}) = 0
 ndims_shape(@nospecialize T::Type{<:AbstractArray{Bool}}) = 1
 ndims_shape(@nospecialize T::Type{<:AbstractArray}) = ndims(T)
 ndims_shape(x) = ndims_shape(typeof(x))
 
-@assume_effects :total function _find_first_true(isi::Tuple{Vararg{Bool,N}}) where {N}
+@assume_effects :total function _find_first_true(isi::Tuple{Vararg{Bool, N}}) where {N}
     for i in 1:N
         getfield(isi, i) && return i
     end
@@ -868,7 +878,7 @@ julia> ndims_index(info)  # assumed no trailing dimensions
 
 ```
 """
-struct IndicesInfo{Np,pdims,cdims,Nc}
+struct IndicesInfo{Np, pdims, cdims, Nc}
     function IndicesInfo{N}(@nospecialize(T::Type{<:Tuple})) where {N}
         SI = _find_first_true(map_tuple_type(is_splat_index, T))
         NI = map_tuple_type(ndims_index, T)
@@ -889,7 +899,7 @@ struct IndicesInfo{Np,pdims,cdims,Nc}
         end
         if ndi === (1,) && N !== 1
             ns1 = getfield(nds, 1)
-            new{N,(:,),(ns1 > 1 ? ntuple(identity, ns1) : ns1,),ns1}()
+            new{N, (:,), (ns1 > 1 ? ntuple(identity, ns1) : ns1,), ns1}()
         else
             nds_cumsum = cumsum(nds)
             if sum(ndi) > N
@@ -907,7 +917,8 @@ struct IndicesInfo{Np,pdims,cdims,Nc}
                 end
                 new{N, pdims, _accum_dims(nds_cumsum, nds), last(nds_cumsum)}()
             else
-                new{N,_accum_dims(cumsum(ndi), ndi), _accum_dims(nds_cumsum, nds), last(nds_cumsum)}()
+                new{N, _accum_dims(cumsum(ndi), ndi), _accum_dims(nds_cumsum, nds),
+                    last(nds_cumsum)}()
             end
         end
     end
@@ -920,7 +931,7 @@ struct IndicesInfo{Np,pdims,cdims,Nc}
         nf = nfields(ndi_sum)
         pdims = _accum_dims(ndi_sum, ndi)
         cdims = _accum_dims(nds_sum, nds)
-        new{getfield(ndi_sum, nf),pdims,cdims,getfield(nds_sum, nf)}()
+        new{getfield(ndi_sum, nf), pdims, cdims, getfield(nds_sum, nf)}()
     end
     IndicesInfo(@nospecialize t::Tuple) = IndicesInfo(typeof(t))
     @inline function IndicesInfo(@nospecialize T::Type{<:SubArray})
@@ -933,7 +944,7 @@ end
         i === splat_index ? (nsplat * getfield(dims, i)) : getfield(dims, i)
     end
 end
-@inline function _accum_dims(csdims::NTuple{N,Int}, nd::NTuple{N,Int}) where {N}
+@inline function _accum_dims(csdims::NTuple{N, Int}, nd::NTuple{N, Int}) where {N}
     ntuple(N) do i
         nd_i = getfield(nd, i)
         if nd_i === 0
@@ -946,7 +957,9 @@ end
     end
 end
 
-_lower_info(::IndicesInfo{Np,pdims,cdims,Nc}) where {Np,pdims,cdims,Nc} = Np,pdims,cdims,Nc
+function _lower_info(::IndicesInfo{Np, pdims, cdims, Nc}) where {Np, pdims, cdims, Nc}
+    Np, pdims, cdims, Nc
+end
 
 ndims_index(@nospecialize(info::IndicesInfo)) = getfield(_lower_info(info), 1)
 ndims_shape(@nospecialize(info::IndicesInfo)) = getfield(_lower_info(info), 4)
@@ -969,7 +982,6 @@ See also: [`IndicesInfo`](@ref), [`parentdims`](@ref)
 """
 childdims(@nospecialize info::IndicesInfo) = getfield(_lower_info(info), 3)
 
-
 """
     instances_do_not_alias(::Type{T}) -> Bool
 
@@ -989,12 +1001,26 @@ That is, would it be safe to write to an array of type `T` in parallel?
 Examples where this is not true are `BitArray`s or `view(rand(6), [1,2,3,1,2,3])`.
 That is, it is not safe whenever different indices may alias the same memory.
 """
-indices_do_not_alias(::Type) = false
-indices_do_not_alias(::Type{A}) where {T, A<:Base.StridedArray{T}} = instances_do_not_alias(T)
-indices_do_not_alias(::Type{Adjoint{T,A}}) where {T, A <: AbstractArray{T}} = indices_do_not_alias(A)
-indices_do_not_alias(::Type{Transpose{T,A}}) where {T, A <: AbstractArray{T}} = indices_do_not_alias(A)
-indices_do_not_alias(::Type{<:SubArray{<:Any,<:Any,A,I}}) where {
-  A,I<:Tuple{Vararg{Union{Integer, UnitRange, Base.ReshapedUnitRange, Base.AbstractCartesianIndex}}}} = indices_do_not_alias(A)
+indices_do_not_alias(::Type{T}) where {T} = _indices_do_not_alias(T)
+function indices_do_not_alias(::Type{A}) where {T, A <: Base.StridedArray{T}}
+    instances_do_not_alias(T)
+end
+function indices_do_not_alias(::Type{Adjoint{T, A}}) where {T, A <: AbstractArray{T}}
+    indices_do_not_alias(A)
+end
+function indices_do_not_alias(::Type{Transpose{T, A}}) where {T, A <: AbstractArray{T}}
+    indices_do_not_alias(A)
+end
+_indices_do_not_alias(::Type) = false
+function _indices_do_not_alias(::Type{<:SubArray{<:Any, <:Any, A, I}}) where
+    {
+     A,
+     I <: Tuple{
+           Vararg{
+                  Union{Integer, UnitRange, Base.ReshapedUnitRange,
+                        Base.AbstractCartesianIndex}}}}
+    indices_do_not_alias(A)
+end
 
 """
     defines_strides(::Type{T}) -> Bool
@@ -1004,7 +1030,7 @@ pointer on `pointer(::T)`.
 """
 defines_strides(x) = defines_strides(typeof(x))
 _defines_strides(::Type{T}, ::Type{T}) where {T} = false
-_defines_strides(::Type{P}, ::Type{T}) where {P,T} = defines_strides(P)
+_defines_strides(::Type{P}, ::Type{T}) where {P, T} = defines_strides(P)
 defines_strides(::Type{T}) where {T} = _defines_strides(parent_type(T), T)
 defines_strides(@nospecialize T::Type{<:StridedArray}) = true
 defines_strides(@nospecialize T::Type{<:BitArray}) = true
