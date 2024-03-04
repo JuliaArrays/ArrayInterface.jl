7c97b62fbb04b4a28b22c0c568b47cfb54f7032e
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-03-03 19:42:48 -0500

    add tests

--- test/ad.jl
+++ test/ad.jl
@@ -1,10 +1,10 @@
-using ArrayInterface, ReverseDiff, Tracker
+using ArrayInterface, ReverseDiff, Tracker, Test
 x = reduce(vcat, ReverseDiff.track([4.0]))
-ArrayInterface.aos_to_soa(x) isa ReverseDiff.TrackedArray
+@test ArrayInterface.aos_to_soa(x) isa ReverseDiff.TrackedArray
 x = reduce(vcat, ReverseDiff.track([4.0,4.0]))
-ArrayInterface.aos_to_soa(x) isa ReverseDiff.TrackedArray
+@test ArrayInterface.aos_to_soa(x) isa ReverseDiff.TrackedArray
 
 x = identity.(Tracker.TrackedArray([4.0]))
-ArrayInterface.aos_to_soa(x) isa Tracker.TrackedArray
+@test ArrayInterface.aos_to_soa(x) isa Tracker.TrackedArray
 x = identity.(Tracker.TrackedArray([4.0,4.0]))
-ArrayInterface.aos_to_soa(x) isa Tracker.TrackedArray
+@test ArrayInterface.aos_to_soa(x) isa Tracker.TrackedArray
