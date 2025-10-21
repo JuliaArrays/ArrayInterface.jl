a266d9f8598294449465521cf157c679ad3fb803
Oscar Smith <oscardssmith@gmail.com>
2025-10-21 17:22:53 -0400

    test CUDSS

--- test/gpu/cuda.jl
+++ test/gpu/cuda.jl
@@ -1,7 +1,20 @@
-using CUDA
+using CUDSS, CUDA, SparseArrays, LinearAlgebra
+using CUDA.CUSPARSE
 using ArrayInterface
 
 using Test
 
+A_cpu = Float32[1 0; 0 1]
+A_dense = CuMatrix(A_cpu)
+A_sparse = CuSparseMatrixCSR(sparse(A_cpu))
+
 # Test whether lu_instance throws an error when invoked with an gpu array
-@test !isa(try ArrayInterface.lu_instance(CUDA.CuArray([1.f0 1.f0; 1.f0 1.f0])) catch ex ex end, Exception) 
+lu_inst_dense = ArrayInterface.lu_instance(A_dense)
+lu_inst_sparse = ArrayInterface.lu_instance(A_sparse)
+
+# test that lu! is valid when using the inst as scratch
+lu_sparse = lu!(lu_inst_sparse, A_sparse)
+
+#test that the resulting lu works
+b = CuVector([1f0, 1f0])
+@test CUDA.@allowscalar lu_sparse \ b == [1, 1]
