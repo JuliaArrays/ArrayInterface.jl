b3e2a79f3ad63aa177295f2fb970065bc26d557a
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-29 03:37:20 -0400

    Preserve tracked arrays in restructure
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- ext/ArrayInterfaceTrackerExt.jl
+++ ext/ArrayInterfaceTrackerExt.jl
@@ -7,10 +7,20 @@ ArrayInterface.ismutable(::Type{<:Tracker.TrackedArray}) = false
 ArrayInterface.ismutable(T::Type{<:Tracker.TrackedReal}) = false
 ArrayInterface.can_setindex(::Type{<:Tracker.TrackedArray}) = false
 ArrayInterface.fast_scalar_indexing(::Type{<:Tracker.TrackedArray}) = false
-ArrayInterface.aos_to_soa(x::AbstractArray{<:Tracker.TrackedReal,N}) where {N} = Tracker.collect(x)
+function ArrayInterface.aos_to_soa(x::AbstractArray{<:Tracker.TrackedReal, N}) where {N}
+    Tracker.collect(x)
+end
 
 function ArrayInterface.restructure(x::Array, y::Tracker.TrackedArray)
     reshape(y, Base.size(x)...)
+end
+function ArrayInterface.restructure(x::Tracker.TrackedArray, y::Tracker.TrackedArray)
+    reshape(y, Base.size(x)...)
+end
+function ArrayInterface.restructure(
+        x::Tracker.TrackedArray, y::AbstractArray{<:Tracker.TrackedReal}
+)
+    reshape(ArrayInterface.aos_to_soa(y), Base.size(x)...)
 end
 function ArrayInterface.restructure(x::Array, y::Array{<:Tracker.TrackedReal})
     reshape(y, Base.size(x)...)
--- test/ad.jl
+++ test/ad.jl
@@ -4,38 +4,50 @@ x = ReverseDiff.track([4.0])
 x = reshape([ReverseDiff.track(rand(1, 1, 1))[1]], 1, 1, 1)
 @test ArrayInterface.aos_to_soa(x) isa ReverseDiff.TrackedArray
 @test ndims(ArrayInterface.aos_to_soa(x)) == 3
-x = reduce(vcat, ReverseDiff.track([4.0,4.0]))
+x = reduce(vcat, ReverseDiff.track([4.0, 4.0]))
 @test ArrayInterface.aos_to_soa(x) isa ReverseDiff.TrackedArray
 x = [ReverseDiff.track([4.0])[1]]
 @test ArrayInterface.aos_to_soa(x) isa ReverseDiff.TrackedArray
-x = reduce(vcat, ReverseDiff.track([4.0,4.0]))
-x = [x[1],x[2]]
+x = reduce(vcat, ReverseDiff.track([4.0, 4.0]))
+x = [x[1], x[2]]
 @test ArrayInterface.aos_to_soa(x) isa ReverseDiff.TrackedArray
 
 x = Tracker.TrackedArray([4.0])
 @test ArrayInterface.aos_to_soa(x) isa Tracker.TrackedArray
 x = [Tracker.TrackedArray([4.0])[1]]
 @test ArrayInterface.aos_to_soa(x) isa Tracker.TrackedArray
-x = Tracker.TrackedArray([4.0,4.0])
+x = Tracker.TrackedArray([4.0, 4.0])
 @test ArrayInterface.aos_to_soa(x) isa Tracker.TrackedArray
-x = reduce(vcat, Tracker.TrackedArray([4.0,4.0]))
-x = [x[1],x[2]]
+x = reduce(vcat, Tracker.TrackedArray([4.0, 4.0]))
+x = [x[1], x[2]]
 @test ArrayInterface.aos_to_soa(x) isa Tracker.TrackedArray
 
 x = rand(4)
-y = Tracker.TrackedReal.(rand(2,2))
+y = Tracker.TrackedReal.(rand(2, 2))
 @test ArrayInterface.restructure(x, y) isa Array
 @test eltype(ArrayInterface.restructure(x, y)) <: Tracker.TrackedReal
 @test size(ArrayInterface.restructure(x, y)) == (4,)
-y = Tracker.TrackedArray(rand(2,2))
+y = Tracker.TrackedArray(rand(2, 2))
+@test ArrayInterface.restructure(x, y) isa Tracker.TrackedArray
+@test size(ArrayInterface.restructure(x, y)) == (4,)
+x = Tracker.TrackedArray(rand(4))
+@test ArrayInterface.restructure(x, y) isa Tracker.TrackedArray
+@test size(ArrayInterface.restructure(x, y)) == (4,)
+y = Tracker.TrackedReal.(rand(2, 2))
 @test ArrayInterface.restructure(x, y) isa Tracker.TrackedArray
 @test size(ArrayInterface.restructure(x, y)) == (4,)
 
 x = rand(4)
-y = ReverseDiff.track(rand(2,2))
+y = ReverseDiff.track(rand(2, 2))
 @test ArrayInterface.restructure(x, y) isa ReverseDiff.TrackedArray
 @test size(ArrayInterface.restructure(x, y)) == (4,)
-y = ReverseDiff.track.(rand(2,2))
+x = ReverseDiff.track(rand(4))
+@test ArrayInterface.restructure(x, y) isa ReverseDiff.TrackedArray
+@test size(ArrayInterface.restructure(x, y)) == (4,)
+y = ReverseDiff.track.(rand(2, 2))
+@test ArrayInterface.restructure(x, y) isa ReverseDiff.TrackedArray
+@test size(ArrayInterface.restructure(x, y)) == (4,)
+x = rand(4)
 @test ArrayInterface.restructure(x, y) isa Array
 @test eltype(ArrayInterface.restructure(x, y)) <: ReverseDiff.TrackedReal
 @test size(ArrayInterface.restructure(x, y)) == (4,)
